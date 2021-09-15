11e7870d4178a9ea53a1604f6111b13b3c091cc4
TSSTECH-B3 <TSSTECH-B3@DESKTOP-QR9J0TK>
2021-09-15 12:16:51 +0900

    プロジェクト ファイルを追加します。

---
+++ CalculateThis/Program.vb
@@ -0,0 +1,16 @@
+Imports System
+
+Module Program
+    Public num1 As Integer
+    Public num2 As Integer
+    Public answer As Integer
+    Sub Main()
+        Console.WriteLine("Type a number and press Enter")
+        num1 = Console.ReadLine()
+        Console.WriteLine("Type another number to add to it and press Enter")
+        num2 = Console.ReadLine()
+        answer = num1 + num2
+        Console.WriteLine("The answer is " & answer)
+        Console.ReadLine()
+    End Sub
+End Module
